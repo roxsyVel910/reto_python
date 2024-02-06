a927541eed69377b2a8f4fed2f28d01b3865843a
roxsyVq <roxsy.dj@gmail.com>
2024-02-05 22:21:58 -0500

    reto uno

---
+++ reto_1.py
@@ -0,0 +1,8 @@
+#reto del dia 1
+
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+nro_telefono = int(input("Ingresa tu numero de telefono: "))
+correo_electronico = input("Ingresa tu correo electronico: ")
+
+print("Hola", nombre, apellido, "en breve recibirás un correo a ", correo_electronico)
